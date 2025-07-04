a34bf818ebf5fadbf37c313ee51589839a404210
mateoXtime <m.fortel@xtime.fr>
2025-07-04 17:39:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,5 @@ def index():
     return "API RAG is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
